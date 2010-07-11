f68ed7d90a6dbb8daf7e3821e65e3a7967b9f64e
Puneet Goyal <pungoyal@gmail.com>
2010-07-11 23:51:20 +0530

    team spec

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,7 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :matches
   has_many :bets
 
-  validates_uniqueness_of :code
-  validates_presence_of :code
+  validates :code, :presence => true, :uniqueness => true
 
   def flag_url
     'flags/' + code + '.png'
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Team do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) { Team.create! :code => 'BRA', :name => 'Brazil' }
+
+  it { should validate_presence_of :code }
+  it { should validate_uniqueness_of :code }
+
+  it { should have_many(:bets) }
+  it { should have_and_belong_to_many(:matches) }
+
+  it "should generate path to the flag image" do
+    Team.first.flag_url.should == 'flags/BRA.png'
+  end
 end
