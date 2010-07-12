6cb57966223ba60ff64b6bae27bd3dc20ff9e9b3
Puneet Goyal <pungoyal@gmail.com>
2010-07-12 08:08:42 +0530

    match spec

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -8,13 +8,28 @@ describe Match do
   it { should have_and_belong_to_many(:teams) }
 
   it "should ensure exactly two teams are involved in a match" do
-    @match.valid?.should == true
+    match = Match.new
 
-    @match.teams << Factory(:team)
-    @match.valid?.should == false
-    @match.errors['teams'].count.should == 1
-    assert_equal 'has 2 teams', @match.errors['teams'].first
+    match.teams << Team.new
+    match.teams.size.should == 1
+    match.valid?.should == false
+    match.errors['teams'].count.should == 1
+    assert_equal 'has 2 teams', match.errors['teams'].first
+
+    match.teams << Team.new
+    match.teams.size.should == 2
+    match.valid?.should == true
+
+    match.teams << Team.new
+    match.teams.size.should == 3
+    match.valid?.should == false
+    match.errors['teams'].count.should == 1
+    assert_equal 'has 2 teams', match.errors['teams'].first
   end
+
+  it "has one of the participating teams as the winner"
+  it "decision updates user points and bets"
+  it "cannot be decided twice"
 
   after(:all) { @match.destroy }
 end
